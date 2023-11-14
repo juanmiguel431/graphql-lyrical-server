e09cca83aa20505184c36a77d5a5d7ecaaa2bff9
juanmiguel431 <juanmiguel431@gmail.com>
2023-11-14 00:14:00 -0400

    cors configured.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
 import 'dotenv/config';
 import './mongoose/connect';
 import express from "express";
+import cors from 'cors';
 import { createHandler } from 'graphql-http/lib/use/express';
 import schema from './graphql/schema';
 
 const app = express();
+app.use(cors({
+  origin: 'http://localhost:5173',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  allowedHeaders: 'Content-Type,Authorization',
+}));
+app.use(express.json());
+
 app.all('/graphql', createHandler({ schema }));
 
 app.get('/', (req, res) => {
